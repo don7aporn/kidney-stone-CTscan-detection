535e85ff42b357786b517149d286dc94fbe4e03f
don7aporn <donnegeegee@gmail.com>
2026-04-10 21:09:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
+
+from ultralytics import YOLO
+import cv2
+
+cv2.setNumThreads(0)
+
 import streamlit as st
 from PIL import Image
-from ultralytics import YOLO
 
 st.title("Kidney Stone Detection")
 
